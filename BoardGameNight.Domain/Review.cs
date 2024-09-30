2bfc926190a37b7f476b34311e1227ee70344a6c
Jochem-tb <88931096+Jochem-tb@users.noreply.github.com>
2024-09-30 17:19:35 +0200

    Create Review.cs

---
+++ BoardGameNight.Domain/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameNight.Domain
+{
+    internal class Review
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Details { get; set; }
+        public required int Rating { get; set; }
+        public required Person Reviewer { get; set; }
+        public required int ReviewerId { get; set; }
+        public required Game Game { get; set; }
+        public required int GameId { get; set; }
+
+    }
+}
