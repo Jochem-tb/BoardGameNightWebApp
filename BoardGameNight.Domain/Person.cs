4890f64f572385ee10b4feaa1091d9aa5135ce66
Jochem-tb <88931096+Jochem-tb@users.noreply.github.com>
2024-09-30 17:01:47 +0200

    Creation Person.cs + Gender.cs

---
+++ BoardGameNight.Domain/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameNight.Domain
+{
+    internal class Person
+    {
+
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public Gender? Gender { get; set; }
+        public int? GenderId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Street { get; set; }
+        public required string HouseNr { get; set; }
+        public required string PostalCode { get; set; }
+        public string? City { get; set; }
+
+    }
+}
