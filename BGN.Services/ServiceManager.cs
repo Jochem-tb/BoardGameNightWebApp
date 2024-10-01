8f0ebf8eff41aeb6256ea7ebbe3d2c4dc98daf0c
Jochem-tb <88931096+Jochem-tb@users.noreply.github.com>
2024-10-01 12:44:39 +0200

    Implement ServiceManager
    Create Dependency classes and projects where needed.

---
+++ BGN.Services/ServiceManager.cs
@@ -0,0 +1,30 @@
+﻿using BGN.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGN.Services
+{
+    internal sealed class ServiceManager : IServiceManager
+    {
+        // Lazy<T> is a thread-safe way to create a singleton
+        //Lazy zorgt ervoor dat de service pas wordt aangemaakt als deze nodig is
+        private readonly Lazy<IPersonService> _lazyPersonService;
+
+
+        // Constructor to inject the repository manager
+
+        //TODO: Implement the constructor
+                //public ServiceManager(IRepositoryManager repositoryManager)
+                //{
+                 //    _lazyPersonService = new Lazy<IPersonService>(() => new PersonService(repositoryManager));
+                 //}
+
+
+        // Property to access the PersonService
+        // This is the only way to access the service
+        public IPersonService PersonService => _lazyPersonService.Value;
+    }
+}
