dc7641a445d1aa469b7c2a04d419fb6dd38b3df9
Jochem-tb <88931096+Jochem-tb@users.noreply.github.com>
2024-10-01 12:57:16 +0200

    Create Infra layer + PersonRepo
    Create Interface for PersonRepo
    Implement basic PersonRepo

---
+++ BGN.Domain/Repositories/IPersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using BGN.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGN.Domain.Repositories
+{
+    public interface IPersonRepository
+    {
+        Task<IEnumerable<Person>> GetAllAsync();
+        Task<Person> GetByIdAsync(int id);
+        void Insert(Person person);
+        void Remove(Person person);
+    }
+}
---
+++ BGN.Infrastructure/Repositories/PersonRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BGN.Domain.Repositories;
+using BGN.Domain.Entities;
+
+namespace BGN.Infrastructure.Repositories
+{
+    internal sealed class PersonRepository : IPersonRepository
+    {
+        private readonly RepositoryDbContext _dbContext;
+
+        public PersonRepository(RepositoryDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public Task<IEnumerable<Person>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Person> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
