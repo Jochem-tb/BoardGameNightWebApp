136f3a4bf639f9aa723f1a3b3396a455e1461f33
Jochem-tb <88931096+Jochem-tb@users.noreply.github.com>
2024-10-02 10:33:28 +0200

    Working on Navbar Dropdowns

---
+++ BGN.UI/ViewComponents/GenreViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BGN.Domain.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+using BGN.Services.Abstractions;
+using BGN.Services;
+
+namespace YourAppNamespace.ViewComponents
+{
+    public class GenreViewComponent : ViewComponent
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public GenreViewComponent(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager; 
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //var genres = await serviceManager; // Fetch genres from database
+            return View(genres);  // Pass the genres to the view
+        }
+    }
+}
